c68a4e7edc9c68e4bb9c677612d9523173423cbf
Mrudhukar <mrudhu@Mrudhukar.(none)>
2009-08-09 00:10:02 +0530

    Authentication using auth logic

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 ActionController::Routing::Routes.draw do |map|
-  # The priority is based upon order of creation: first created -> highest priority.
+  map.resources :users
+  map.resources :user_sessions
+  
+
+  map.login "login", :controller => "user_sessions", :action => "new"
+  map.logout "logout", :controller => "user_sessions", :action => "destroy"
 
   # Sample of regular route:
   #   map.connect 'products/:id', :controller => 'catalog', :action => 'view'
@@ -8,9 +13,6 @@ ActionController::Routing::Routes.draw do |map|
   # Sample of named route:
   #   map.purchase 'products/:id/purchase', :controller => 'catalog', :action => 'purchase'
   # This route can be invoked with purchase_url(:id => product.id)
-
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   map.resources :products
 
   # Sample resource route with options:
   #   map.resources :products, :member => { :short => :get, :toggle => :post }, :collection => { :sold => :get }
@@ -31,7 +33,7 @@ ActionController::Routing::Routes.draw do |map|
   #   end
 
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
-  # map.root :controller => "welcome"
+  map.root :controller => "users", :action => "show"
 
   # See how all your routes lay out with "rake routes"
 
--- db/migrate/20090808102911_create_users.rb
+++ db/migrate/20090808102911_create_users.rb
@@ -6,6 +6,7 @@ class CreateUsers < ActiveRecord::Migration
       t.string    :crypted_password,   :null => false
       t.string    :password_salt,      :null => false
       t.string    :persistence_token,  :null => false
+      t.string    :perishable_token,   :null => false, :default => ""
 
       t.integer   :login_count,         :null => false, :default => 0 # optional, see Authlogic::Session::MagicColumns
       t.integer   :failed_login_count,  :null => false, :default => 0 # optional, see Authlogic::Session::MagicColumns
