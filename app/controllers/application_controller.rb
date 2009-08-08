2995741a2ed71b98d6dc4bf532b7a37de8fcbf90
Mrudhukar <mrudhu@Mrudhukar.(none)>
2009-08-09 00:08:18 +0530

    Fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,5 +6,28 @@ class ApplicationController < ActionController::Base
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
 
   # Scrub sensitive parameters from your log
-  # filter_parameter_logging :password
+  filter_parameter_logging :password
+  before_filter :require_login
+  
+  helper_method :current_user
+    
+  protected
+
+  def current_user_session
+    return @current_user_session if defined?(@current_user_session)
+    @current_user_session = UserSession.find
+  end
+
+  def current_user
+    return @current_user if defined?(@current_user)
+    @current_user = (current_user_session && current_user_session.record)
+  end
+
+  def require_login
+    unless current_user_session
+      session[:orginal_uri] = request.request_uri
+      redirect_to login_path
+    end
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,40 @@
 class UsersController < ApplicationController
+  skip_before_filter :require_login, :only => [:new, :create]
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      redirect_to user_url(@user)
+    else
+      render :action => "new"
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      flash[:notice] = "Successfull updated your profile"
+      redirect_to user_path(@user)
+    else
+      render :action => "edit"
+    end
+  end
+
+  def show
+
+  end
+
+  def index
+    
+  end
+
+
 end
